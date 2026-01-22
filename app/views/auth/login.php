ad4a49c64679044782b36b6b1f658577d78848e7
Enric Presti <165492761+enerico123@users.noreply.github.com>
2026-01-22 21:23:16 +0100

    premier commit STRUCTURE

---
+++ app/views/auth/login.php
@@ -0,0 +1,5 @@
+<form method="POST" action="/login">
+    <input type="text" name="username" placeholder="Nom d'utilisateur" required>
+    <input type="password" name="password" placeholder="Mot de passe" required>
+    <button type="submit">Connexion</button>
+</form>
